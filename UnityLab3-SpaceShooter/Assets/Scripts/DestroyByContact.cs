b9148243f68c4e762b4a5561909953d1874cd01c
Emily Barnett <K00198630@student.lit.ie>
2018-10-15 17:34:03 +0100

    Completed Space Shooter

---
+++ UnityLab3-SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);
+        if (other.tag == "Boundary")
+        {
+            return;//ends function, continues back through code
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);//destroys other object in collision
+        Destroy(gameObject);//destroys own object - asteroid and all children
+    }
+}
---
+++ UnityLab3-SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]//ensures the class is serialized and visible
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    private void Update()
+    {
+        while (Input.GetKey("space") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //tells unity to instantiate this object as a game object
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();//plays audio
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Grabs the input from the player - default axis preset in the input manager
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Apply the input to the player game object - non realistic physics behaviour
+        //Address rigidbody component
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // GetComponent<Rigidbody>();
+        GetComponent<Rigidbody>().velocity = movement * speed;//velocity uses a vector 3 value, direction we are travelling + how fast as vector and its magnitude
+
+        //Set player position back to edge of game area
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        );
+
+        //tilt the ship using the velocity on the x axis
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
